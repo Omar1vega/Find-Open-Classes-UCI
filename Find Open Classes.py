c35b5b496d0d784701c5069cccd6f3f01b3570c3
omar1vega <omar1vega@gmail.com>
2019-02-18 23:16:30 -0800

    applying pep8 standards

--- Find Open Classes.py
+++ Find Open Classes.py
@@ -1,18 +1,16 @@
 import smtplib
-import requests
 import time
+
+import requests
 
 """ Written By Omar Vega
     https://github.com/Omar1vega/
 """
 
 
-def sendText(content, recipient):
-    username = "[User_Email]@gmail.com"  ##enter gmail username and password to send text to mobile
+def send_text(message, recipient):
+    username = "[User_Email]@gmail.com"  # enter gmail username and password to send text to mobile
     password = "[User Password]"
-
-    recipient = recipient
-    message = content
 
     msg = """From: %s \nTo: %s\nSubject:FindOpenClasses\n%s""" % (username, recipient, message)
 
@@ -22,50 +20,56 @@ def sendText(content, recipient):
     server.starttls()
     server.login(username, password)
     server.sendmail(username, recipient, msg)
-    print("done")
     server.quit()
 
 
 def main():
-    searchType = input(
-        " Type 'C' to search by Course Code (e.g. 54000) \n Type 'D' to search by Department+Course (e.g. Writing 39c)\n ")
-    if (searchType.upper() == 'C'):
-        courseCode = input("Please Enter The 5 Digit Course Code")
-        url = "https://www.reg.uci.edu/perl/WebSoc/?YearTerm=2016-14&ShowComments=on&ShowFinals=on&Breadth=ANY&Dept=+ALL&CourseNum=&Division=ANY&CourseCodes=" + courseCode + "&InstrName=&CourseTitle=&ClassType=ALL&Units=&Days=&StartTime=&EndTime=&MaxCap=&FullCourses=ANY&FontSize=100&CancelledCourses=Exclude&Bldg=&Room=&Submit=Display+Text+Results"
+    search_type = input(
+        "Type 'C' to search by Course Code (e.g. 54000) \n Type 'D' to search by Department+Course (e.g. Writing "
+        "39c)\n ")
+    if search_type.upper() == 'C':
+        course_code = input("Please Enter The 5 Digit Course Code")
+        url = "https://www.reg.uci.edu/perl/WebSoc/?YearTerm=2016-14&ShowComments=on&ShowFinals=on&Breadth=ANY&Dept" \
+              "=+ALL&CourseNum=&Division=ANY&CourseCodes=" + course_code + \
+              "&InstrName=&CourseTitle=&ClassType=ALL&Units=&Days=&StartTime=&EndTime=&MaxCap=&FullCourses=ANY" \
+              "&FontSize=100&CancelledCourses=Exclude&Bldg=&Room=&Submit=Display+Text+Results "
 
-    elif (searchType.upper() == 'D'):
+    elif search_type.upper() == 'D':
         dept = input("Please Enter the department name (e.g. ""Writing"",or ""EECS"") \n ").strip()
         course = input("Please Enter the Course (e.g. ""39C"",or ""20"") \n ").strip()
-        url = 'https://www.reg.uci.edu/perl/WebSoc/?YearTerm=2016-14&ShowComments=on&ShowFinals=on&Breadth=ANY&Dept=' + dept.upper() + '&CourseNum=' + course + '&Division=ANY&CourseCodes=&InstrName=&CourseTitle=&ClassType=ALL&Units=&Days=&StartTime=&EndTime=&MaxCap=&FullCourses=ANY&FontSize=100&CancelledCourses=Exclude&Bldg=&Room=&Submit=Display+Text+Results'
+        url = 'https://www.reg.uci.edu/perl/WebSoc/?YearTerm=2016-14&ShowComments=on&ShowFinals=on&Breadth=ANY&Dept=' \
+              + dept.upper() + '&CourseNum=' + course \
+              + '&Division=ANY&CourseCodes=&InstrName=&CourseTitle=&ClassType=ALL&Units=&Days=&StartTime=&EndTime' \
+                '=&MaxCap=&FullCourses=ANY&FontSize=100&CancelledCourses=Exclude&Bldg=&Room=&Submit=Display+Text' \
+                '+Results '
 
     else:
         main()
 
-    classList = []
-    phoneNum = input("Phone Number? ").strip()
+    class_list = []
+    phone_number = input("Phone Number? ").strip()
     carrier = input(
-        "Carrier? 'Al' for Alltel \n 'ATT' for AT&T \n  'B' for Boost Mobile \n 'S' for Sprint \n 'T' for T-Mobile \n 'Vz' for Verizon \n 'Vm' for Virgin Mobile ")
+        "Carrier? 'Al' for Alltel \n 'ATT' for AT&T \n  'B' for Boost Mobile \n 'S' for Sprint \n 'T' for T-Mobile \n "
+        "'Vz' for Verizon \n 'Vm' for Virgin Mobile ")
 
-    if (carrier.upper() == 'AL'):
+    if carrier.upper() == 'AL':
         carrier = "@message.alltel.com"
-    elif (carrier.upper() == 'ATT'):
+    elif carrier.upper() == 'ATT':
         carrier = "@txt.att.net"
-    elif (carrier.upper() == 'B'):
+    elif carrier.upper() == 'B':
         carrier = "@myboostmobile.com"
-    elif (carrier.upper() == 'S'):
+    elif carrier.upper() == 'S':
         carrier = "@messaging.sprintpcs.com"
-    elif (carrier.upper() == 'T'):
+    elif carrier.upper() == 'T':
         carrier = "@tmomail.net"
-    elif (carrier.upper() == 'VZ'):
+    elif carrier.upper() == 'VZ':
         carrier = "@vtext.com"
-    elif (carrier.upper() == 'VM'):
+    elif carrier.upper() == 'VM':
         carrier = "@vmobl.com"
-    recipient = phoneNum + carrier
+    recipient = phone_number + carrier
 
-    while True:  ## rechecks every 60s
-
-        r = requests.get(url)
-        classes = r.text
+    while True:  # rechecks every 60s
+        classes = requests.get(url).text
 
         file = open('classes.txt', 'w')
         file.write(classes)
@@ -78,18 +82,18 @@ def main():
             line = line.split()
             if (line[-1] != "FULL") and line[0].isdigit():
                 if line[-1] == 'Open':
-                    classList.append(line[0] + '(Open)')
+                    class_list.append(line[0] + '(Open)')
                 else:
                     print(line[0])
-                    classList.append(line[0])
+                    class_list.append(line[0])
 
-        classString = ', '.join(classList)
-        classString = dept + course + " Classes Available " + classString
-        print(classString)
+        class_string = ', '.join(class_list)
+        class_string = dept + course + " Classes Available " + class_string
+        print(class_string)
 
-        if len(classList) > 0:
-            sendText(str(classString), recipient)
-        time.sleep(60)  ## rechecks every 60s
+        if len(class_list) > 0:
+            send_text(str(class_string), recipient)
+        time.sleep(60)  # rechecks every 60s
 
 
 main()
